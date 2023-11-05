67be5ad43f7ff717079957f59f68b2b65b6e48a4
Re Julian <julianre2021@gmail.com>
2023-11-05 17:24:30 -0300

    Agregando logica de cart

--- proyecto-final/src/index.js
+++ proyecto-final/src/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import  { productsRouter }  from './routes/products.router.js';
 import  { cartsRouter }  from './routes/carts.router.js';
 import { ProductManager } from './productManager.js';
+import { CartManager } from './cartManager.js';
+
 const PORT = 8080;
 
 const app = express();
 
 export const productManager = new ProductManager;
+export const cartManager = new CartManager;
 
 app.use(express.json())
 app.use('/api/products', productsRouter)
--- proyecto-final/src/routes/carts.router.js
+++ proyecto-final/src/routes/carts.router.js
@@ -1,9 +1,38 @@
 import { Router } from "express";
+import { cartManager } from "../index.js"
+
 
 const cartsRouter = Router()
 
-cartsRouter.get('/', (req, res) => {
-    res.send('carrito')
+cartsRouter.post('/', async (req, res) => {
+    try {
+        const response = await cartManager.newCart()
+        res.json(response)
+    } catch (error) {
+        console.log('Error al crear carrito');
+    }
+})
+
+cartsRouter.get('/:cid', async (req, res) => {
+    const {cid} = req.params;
+
+    try {
+      const response = await cartManager.getCartProducts(cid)
+      res.json(response)
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+cartsRouter.post('/:cid/product/:pid', async (req, res) => {
+    const {cid, pid} = req.params;
+
+    try {
+        await cartManager.addProductToCart(cid, pid)
+        res.send('Producto agregado exitosamente')
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 export {cartsRouter}
